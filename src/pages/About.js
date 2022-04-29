1a7743f23152d75b15b5cb6428353b09625771e3
mayowa86 <mayowasamuel86@gmail.com>
2022-04-29 05:23:08 +0100

    updates

---
+++ src/pages/About.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const About = () => {
+	return (
+		<section className="section about-section">
+			<h1 className="section-title">About us</h1>
+			<p>
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab, eius sed.
+				Temporibus perspiciatis nulla nostrum ipsam sit, veritatis natus.
+				Tempora repellendus labore eos! Consequatur quasi error aperiam sit
+				deleniti culpa omnis voluptate? Molestias provident obcaecati eligendi.
+				Laboriosam exercitationem ipsum vel!
+			</p>
+		</section>
+	);
+};
+
+export default About;
